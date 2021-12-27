6fad28b72ae13c8fd95be396bdd9d63e97ddaa8a
Iqbalmubarak <48642060+Iqbalmubarak@users.noreply.github.com>
2021-12-27 11:11:04 +0700

    FavoriteMenu

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Restaurant;
-use App\Models\FavoriteRestaurant;
+use App\Models\FavoriteMenu;
 use DB;
 
 class RestaurantController extends Controller
--- app/Models/FavoriteMenu.php
+++ app/Models/FavoriteMenu.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
-class FavoriteRestaurant extends Model 
+class FavoriteMenu extends Model 
 {
     use HasFactory;
 
